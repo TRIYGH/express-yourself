2de0c9a3abd5c50a4a5cb7574d989e79695625a7
T. Robert Ward <trironyard@icloud.com>
2016-08-18 08:54:10 -0400

    minor change

--- textminer/extractor.py
+++ textminer/extractor.py
@@ -1,6 +1,5 @@
 import re
 
 
-def phone_number(data):
-    if re.search(r'.*\d{3}.*\d{3}.*\d{4}', data):
-        return True
+def phone_numbers(data):
+    return re.findall(r'.?\d{3}.*\d{3}.*\d{4}', data)
